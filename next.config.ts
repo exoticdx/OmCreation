cfede704c836502de7b1bf21edc5c092e742743a
DAKSH MUNGALPARA <dakshmungalpara4@gmail.com>
2026-09-17 10:55:12 +0530

    Increase body size limit for 10mb image uploads

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
   allowedDevOrigins: [
     '192.168.29.171', 'http://192.168.29.171:3000',
     '192.168.1.54', 'http://192.168.1.54:3000',
